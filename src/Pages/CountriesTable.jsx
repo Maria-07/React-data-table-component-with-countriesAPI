8c5cc1694367a498909ef3199824ccb50578b244
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-06-05 00:52:03 +0600

    filter data from react data component

--- src/Pages/CountriesTable.jsx
+++ src/Pages/CountriesTable.jsx
@@ -4,12 +4,15 @@ import DataTable from "react-data-table-component";
 
 const CountriesTable = () => {
   const [countries, setCountries] = useState([]);
+  const [filterCountries, setFilterCountries] = useState([]);
+  const [search, setSearch] = useState("");
 
   const getCountries = async () => {
     try {
       const url = await axios.get("https://restcountries.com/v2/all");
       // .then((data) => setCountries(data.data));
       setCountries(url.data);
+      setFilterCountries(url.data);
     } catch (error) {
       console.log(error);
     }
@@ -19,6 +22,7 @@ const CountriesTable = () => {
     {
       name: "Country Name",
       selector: (row) => row.name,
+      sortable: true,
     },
     {
       name: "Native Name",
@@ -32,19 +36,53 @@ const CountriesTable = () => {
       name: "Country Flag",
       selector: (row) => <img width={50} height={50} src={row.flag} alt="" />,
     },
+    {
+      name: "Action",
+      cell: (row) => (
+        <button
+          className="btn btn-primary"
+          onClick={() => alert(row.numericCode)}
+        >
+          Edit
+        </button>
+      ),
+    },
   ];
 
   useEffect(() => {
     getCountries();
   }, []);
+
+  useEffect(() => {
+    const result = countries.filter((country) => {
+      return country.name.toLowerCase().match(search.toLowerCase());
+    });
+    setFilterCountries(result);
+  }, [countries, search]);
+
   return (
     <DataTable
       title="Countries List"
       columns={columns}
-      data={countries}
+      data={filterCountries}
       pagination
       fixedHeader
       fixedHeaderScrollHeight="650px"
+      selectableRows
+      selectableRowsHighlight
+      highlightOnHover
+      actions={<button className="btn btn-info"> Export </button>}
+      subHeader
+      subHeaderComponent={
+        <input
+          type="text"
+          placeholder="Search here"
+          className="w-25 form-control"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        ></input>
+      }
+      //   subHeaderAlign="left"
     ></DataTable>
   );
 };
